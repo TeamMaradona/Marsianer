0381ef15cf4a8bb71ae89452fa400a493b7df7e1
Lunix-420 <davidhess336@gmx.de>
2024-09-22 20:41:40 +0200

    feat: Messungs Stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,34 @@
 #include <Arduino.h>
 
+// Werte aus der Kalibrierung
+const int AirValue = 824;
+const int WaterValue = 477;
+
+// Intervalle für die Ausgabe
+int intervals = (AirValue - WaterValue) / 3;
+int soilMoistureValue = 0;
+
+// Hier wird die Funktion der Pins festgelegt
 void
 setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
 }
 
+// Hauptprogramm
 void
 loop()
 {
-  int val;
-  val = analogRead(0);
-  Serial.println(val); // print the value to serial port
-  delay(1000);         // wait for 100ms
+  soilMoistureValue = analogRead(A0);
+  if (soilMoistureValue > WaterValue &&
+      soilMoistureValue < (WaterValue + intervals)) {
+    Serial.println("Very Wet");
+  } else if (soilMoistureValue > (WaterValue + intervals) &&
+             soilMoistureValue < (AirValue - intervals)) {
+    Serial.println("Wet");
+  } else if (soilMoistureValue < AirValue &&
+             soilMoistureValue > (AirValue - intervals)) {
+    Serial.println("Dry");
+  }
+  delay(100);
 }
