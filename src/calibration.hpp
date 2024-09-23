9bf89e6fe762492c503a0a638679a0e45ebcd6b2
Lunix-420 <davidhess336@gmx.de>
2024-09-23 10:28:26 +0200

    docs: Doxygen Kommentare

--- src/calibration.hpp
+++ src/calibration.hpp
@@ -1,7 +1,17 @@
 #pragma once
 #include <Arduino.h>
 
+/**
+ * @file calibration.hpp
+ * @brief Enthält Funktionen zur Kalibrierung des Bodenfeuchtigkeitssensors.
+ */
 namespace calibration {
+
+/**
+ * @brief Liest den aktuellen Sensorwert des analogen Pins 0 und gibt ihn über
+ * USB aus. Diese Funktion wird verwendet, um die Kalibrierungswerte für Luft
+ * und Wasser zu bestimmen. Die Funktion wartet 1000ms zwischen den Messungen.
+ */
 void
 print()
 {
--- src/moisture.hpp
+++ src/moisture.hpp
@@ -1,15 +1,54 @@
 #pragma once
 #include <Arduino.h>
-namespace moisture {
-// Werte aus der Kalibrierung
-const int AirValue = 824;
-const int WaterValue = 477;
 
+/**
+ * @file moisture.hpp
+ * @brief Enthält Funktionen zur Bestimmung der Bodenfeuchtigkeit.
+ */
+namespace moisture {
+
+/**
+ * @brief Der Kalibrierungswert für Luft.
+ *
+ * Dieser Wert wird verwendet, um den Bereich der Bodenfeuchtigkeit zu
+ * kalibrieren. Der Wert wird durch Messung der Bodenfeuchtigkeit in der Luft
+ * bestimmt.
+ *
+ */
+static inline int AirValue = 800;
+
+/**
+ * @brief Der Kalibrierungswert für Wasser.
+ *
+ * Dieser Wert wird verwendet, um den Bereich der Bodenfeuchtigkeit zu
+ * kalibrieren. Der Wert wird durch Messung der Bodenfeuchtigkeit in Wasser
+ * bestimmt.
+ *
+ */
+static inline int WaterValue = 400;
+
+/**
+ * @brief Berechnet das Intervall der Bodenfeuchtigkeit basierend auf der
+ * Kalibrierung.
+ *
+ * Diese Funktion liest den aktuellen Bodenfeuchtigkeitswert von einem analogen
+ * Pin und berechnet das entsprechende Intervall basierend auf den
+ * Kalibrierungswerten für Luft und Wasser. Die Intervalle werden gleichmäßig
+ * zwischen den Kalibrierungswerten aufgeteilt.
+ *
+ * @param numIntervals Die Anzahl der Intervalle, in die der Bereich zwischen
+ * den Kalibrierungswerten aufgeteilt werden soll.
+ * @return Das Intervall, in dem der aktuelle Bodenfeuchtigkeitswert liegt. Wenn
+ * der Wert außerhalb der Kalibrierungswerte liegt, wird das höchste Intervall
+ * zurückgegeben.
+ */
 const int
 getInterval(const int numIntervals)
 {
   // Intervalle für die Ausgabe berechnen
   const int intervals = (AirValue - WaterValue) / numIntervals;
+
+  // Sensorwert lesen
   const int soilMoistureValue = analogRead(A0);
 
   // Berechnung des Feuchtigkeitswertes
